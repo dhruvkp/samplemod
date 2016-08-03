d4e78bae534d443be55a70bf3ab0a4a90ded70e2
Dhruv Patel <dhruvkumarr.patel51@gmail.com>
2016-08-02 19:44:52 -0500

    First commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+Sample Module Repository
+========================
+
+This simple project is an example repo for Python projects.
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sample',
+    version='0.0.1',
+    description='Sample package',
+    long_description=readme,
+    author='Dhruv Patel',
+    author_email='dhruvkumarr.patel51@gmail.com',
+    url='https://github.com/dhruvkp/',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
